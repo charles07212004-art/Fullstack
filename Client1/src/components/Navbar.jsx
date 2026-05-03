9b7150165f6a8a98735b426d654a93a17490791f
charles07212004-art <charles07212004@gmail.com>
2026-05-03 21:18:31 +0800

    Change logo text from 'NewTube' to 'ThunderTUBE'

--- Client1/src/components/Navbar.jsx
+++ Client1/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = ({ onMenuClick }) => {
         </button>
         <Link to="/" className="logo">
           
-          <span>NewTube</span>
+          <span>ThunderTUBE</span>
         </Link>
       </div>
 
